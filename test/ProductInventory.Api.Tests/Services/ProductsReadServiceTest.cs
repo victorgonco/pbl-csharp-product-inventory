ff4bd8a500683a1034c315f4f367b7ee77e9bc9f
Gonco <gonco.dev@outlook.com>
2026-03-29 11:13:20 -0300

    feat: Lista de TODOs no ProducsReadServiceTest

--- test/ProductInventory.Api.Tests/Services/ProductsReadServiceTest.cs
+++ test/ProductInventory.Api.Tests/Services/ProductsReadServiceTest.cs
@@ -6,7 +6,11 @@ namespace ProductInventory.Api.Tests.Services
 {
     public class ProductsReadServiceTest
     {
-
+        // TODO: Seguir adicionando testes desta classe para usar TDD como guia.
+        // TODO: A partir daqui, criar o teste de controoller, criar a controller e ir seguindo
+        // TODO: Setar um teste integrado para consultar o banco, quebrar o teste e depois colocar o contexto do dapper
+        // Nota: A idéia é forçar o desenvolvimento a ser guiado pela funcionalidade desenhada, que é o crud descrito no readme.md
+        // Já pensando no que precisa ser resultado das rotinas para poder fazer a funcionalidade acontecer.
         [Fact]
         public void Should_Return_Values_When_Called()
         {
